76fcdbb4a795c24a253f9c4225187a41ca45bc83
Bhavin IIH <bhavin.iihglobal@gmail.com>
2022-11-04 18:40:13 +0530

    prevent empty data api calling

--- services/healthloq.js
+++ services/healthloq.js
@@ -3,21 +3,22 @@ const { generateJwtToken } = require("../utils");
 
 exports.syncHash = async (data) => {
   try {
-    console.log(data);
-    console.log("Start syncing with healthloq db...");
-    const token = generateJwtToken();
-    const response = await axios.post(
-      `${process.env.HEALTHLOQ_API_BASE_URL}/document-hash/createOrDelete`,
-      data,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    if (response.data.status === "1")
-      console.log("Hash synced with healthloq successful");
-    else console.log(response.data.message);
+    if (data.hashList.length || data.deletedHashList.length) {
+      console.log("Start syncing with healthloq db...");
+      const token = generateJwtToken();
+      const response = await axios.post(
+        `${process.env.HEALTHLOQ_API_BASE_URL}/document-hash/createOrDelete`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.data.status === "1")
+        console.log("Hash synced with healthloq successful");
+      else console.log(response.data.message);
+    }
   } catch (error) {
     console.log("sync hash with healthloq catch block", error);
   }
