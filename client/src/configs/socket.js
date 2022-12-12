9102b5b0de5b0e0cc0b626949e645e2a325eb3bb
Ronak IIH <ronak.patel@genbloq.com>
2022-12-12 16:10:07 +0530

    update socket config method

--- client/src/configs/socket.js
+++ client/src/configs/socket.js
@@ -1,8 +1,12 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_API_BASE_URL, {
-  auth: {
-    token: process.env.REACT_APP_JWT_TOKEN,
-  },
-  transports: ["websocket"],
+export const socket = io.connect(process.env.REACT_APP_API_BASE_URL, {
+  auth: { token: process.env.REACT_APP_JWT_TOKEN },
 });
+
+// export const socket = io(process.env.REACT_APP_API_BASE_URL, {
+//   auth: {
+//     token: process.env.REACT_APP_JWT_TOKEN,
+//   },
+//   transports: ["websocket"],
+// });
