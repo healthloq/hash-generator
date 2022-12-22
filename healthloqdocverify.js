f4c4ab701ce362e6b51d2a8e2605fbda79c81785
Ronak IIH <ronak.patel@genbloq.com>
2022-12-22 19:33:26 +0530

    create build

--- healthloqdocverify.js
+++ healthloqdocverify.js
@@ -72,10 +72,10 @@ io.on("connection", (socket) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/public", express.static(path.join(__dirname, "./public")));
+// app.use("/public", express.static(path.join(__dirname, "./public")));
 app.use("/api/client", require("./routes/client"));
 
-app.use("/client", express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client/build/index.html"));
 });
